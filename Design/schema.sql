e4cf8a9c93502e476153b6d614af72e99e9d453f
Harrison Slater <akand_@hotmail.com>
2015-08-04 17:19:36 +1000

    Added planning and design folders

---
+++ Design/schema.sql
@@ -0,0 +1,74 @@
+USE DATABASE forum_db;
+DROP TABLE moderators_forums;
+DROP TABLE moderators;
+DROP TABLE administrators;
+DROP TABLE comments;
+DROP TABLE threads;
+DROP TABLE forums;
+DROP TABLE users;
+
+CREATE TABLE users
+(
+id INT NOT NULL AUTO_INCREMENT,
+username VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+email VARCHAR(254) NOT NULL,
+created DATE NOT NULL,
+PRIMARY KEY (id) 
+);
+
+CREATE TABLE forums
+(
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(255) NOT NULL,
+created DATE NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE threads
+(
+id INT NOT NULL AUTO_INCREMENT,
+forum_id INT NOT NULL,
+title VARCHAR(255) NOT NULL,
+created DATE NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (forum_id) REFERENCES forums(id)
+);
+
+CREATE TABLE comments
+(
+id INT NOT NULL AUTO_INCREMENT,
+thread_id INT NOT NULL,
+user_id INT NOT NULL,
+text VARCHAR(10000),
+created DATETIME NOT NULL,
+modified DATETIME,
+PRIMARY KEY (id),
+FOREIGN KEY (thread_id) REFERENCES threads(id),
+FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE administrators
+(
+user_id INT NOT NULL,
+PRIMARY KEY (user_id),
+FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE moderators
+(
+user_id INT NOT NULL,
+PRIMARY KEY (user_id),
+FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE moderators_forums
+(
+id INT NOT NULL AUTO_INCREMENT,
+user_id INT NOT NULL,
+forum_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (user_id) REFERENCES moderators(user_id),
+FOREIGN KEY (forum_id) REFERENCES forums(id)
+);
+
